671c298273e6904e9747b1dffcb4c18ebf7c0aa0
Siddharth2207 <siddharthbhoite22@gmail.com>
2024-02-06 02:42:09 +0530

    ieon setup

---
+++ test/TwapTrendTest.t.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: CAL
+pragma solidity =0.8.19;
+
+import {Vm} from "forge-std/Vm.sol";
+import {console2, Test} from "forge-std/Test.sol";
+
+contract TwapTrendTest is Test {
+    
+}
+
